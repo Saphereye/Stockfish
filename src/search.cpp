2c28610aace1c1afeb1498fe514fa2e1da739da9
Adarsh Das <adarshdas950@gmail.com>
2026-04-04 21:45:36 +0530

    init
    When the opponent was in check last move, their staticEval was set to
    the value from 2 moves before them (4 plies ago total). So the
    opponentWorsening eval was stale eval from a different position.

--- src/search.cpp
+++ src/search.cpp
@@ -765,7 +765,7 @@ Value Search::Worker::search(
     // Similarly, opponentWorsening is true if our static evaluation is better
     // for us than at the last ply.
     improving         = ss->staticEval > (ss - 2)->staticEval;
-    opponentWorsening = ss->staticEval > -(ss - 1)->staticEval;
+    opponentWorsening = !(ss-1)->inCheck && ss->staticEval > -(ss-1)->staticEval;
 
     // Hindsight adjustment of reductions based on static evaluation difference.
     if (priorReduction >= 3 && !opponentWorsening)
