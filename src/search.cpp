b5f5ea3b7ba9791118f1dba62722c6e9321905dc
Adarsh Das <adarshdas950@gmail.com>
2026-08-08 18:15:27 +0530

    Take 1
    Bench: 2646819

--- src/search.cpp
+++ src/search.cpp
@@ -1178,7 +1178,8 @@ moves_loop:  // When in check, search starts here
                 // SEE based pruning for captures and checks
                 // Avoid pruning sacrifices of our last piece for stalemate
                 int margin = 177 * depth + captHist * 34 / 1024;
-                if ((alpha >= VALUE_DRAW || pos.non_pawn_material(us) != PieceValue[movedPiece])
+                if (!ss->inCheck
+                    && (alpha >= VALUE_DRAW || pos.non_pawn_material(us) != PieceValue[movedPiece])
                     && !pos.see_ge(move, -margin))
                     continue;
             }
